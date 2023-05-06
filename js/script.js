a8f0fa3d7af1800156da42645f89d185712c2600
Raja-Junaid <rajajunaidpdk@gmail.com>
2023-05-06 15:43:36 +0500

    Just few changes

--- js/script.js
+++ js/script.js
@@ -5,12 +5,11 @@ const navMenu = document.querySelector(".modal");
 hamburger.addEventListener("click", function () {
   navMenu.classList.remove("hidden");
   navMenu.classList.add("flex");
-  navMenu.classList.add("show");
 });
 
 closeBtn.addEventListener("click", function () {
-  navMenu.classList.remove("show");
   navMenu.classList.add("hidden");
+  navMenu.classList.remove("flex");
 });
 
 navMenu.addEventListener("click", function () {
