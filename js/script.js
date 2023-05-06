9ba30311180f7db7e0148b397fa8ea003d2e5618
Raja-Junaid <rajajunaidpdk@gmail.com>
2023-05-06 14:35:22 +0500

    Added navbar Animation

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const closeBtn = document.querySelector(".close");
+const navMenu = document.querySelector(".modal");
+
+hamburger.addEventListener("click", function () {
+  navMenu.classList.remove("hidden");
+  navMenu.classList.add("flex");
+});
+
+closeBtn.addEventListener("click", function () {
+  navMenu.classList.add("hidden");
+  navMenu.classList.remove("flex");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        white: "#ffffff",
+        White: "#ffffff",
         veryPaleBlue: "#f6f9fe",
         deepPurple: "#372865",
         darkSlateBlue: "#353252",
         darkGray: "#222222",
-        languidLavender: "#ccc1e6"
-      }
+        lightGray: "#444444",
+        languidLavender: "#ccc1e6",
+      },
     },
   },
   plugins: [],
-}
-
+};
