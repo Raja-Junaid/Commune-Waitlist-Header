bf6919cf20018955d0509d5e62507d8bd318e01c
Raja-Junaid <rajajunaidpdk@gmail.com>
2023-05-06 12:56:13 +0500

    Add colors in tailwind.config.js and add font family in index and input

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#ffffff",
+        veryPaleBlue: "#f6f9fe",
+        deepPurple: "#372865",
+        darkSlateBlue: "#353252",
+        darkGray: "#222222",
+        languidLavender: "#ccc1e6"
+      }
+    },
   },
   plugins: [],
 }
